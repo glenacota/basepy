1c719b71e6b21de762ea4c3b3a9ab5763584334b
glenacota <guido.lenacota@gmail.com>
2020-11-29 16:35:52 +0100

    Add main package code

---
+++ basepy/__main__.py
@@ -0,0 +1,13 @@
+"""Package entry point
+"""
+
+import basepy
+
+
+def main():
+    """ Run basepy script """
+    basepy
+
+
+if __name__ == "__main__":
+    main()
---
+++ basepy/basepy.py
@@ -0,0 +1,4 @@
+"""Greet the user
+"""
+
+print("Hi, you!")
