355ae2892b2d74a185e5f4e185d58343d35d20a9
glenacota <guido.lenacota@gmail.com>
2020-11-29 16:34:59 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="basepy",
+    version="0.0.1",
+    description="Opinionated layout for Python 3 projects",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Guido Lena Cota",
+    url="https://github.com/glenacota/basepy",
+    license=license,
+    install_requires=[],
+    packages=find_packages(exclude=("tests", "docs")),
+)
