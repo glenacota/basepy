58ca07f6bbbaf6f48b4cd18beeea23800495d659
glenacota <guido.lenacota@gmail.com>
2020-11-30 09:54:44 +0100

    Create CLI to output greetings

--- basepy/basepy.py
+++ basepy/basepy.py
@@ -1,9 +1,27 @@
 """Dummy module
 """
 
+import click
 from libs.logger import log
 
 
+@click.group()
 def run():
-    log().info("All is good!")
-    return True
+    """ A simple CLI """
+    pass
+
+
+@run.command()
+@click.option("--names", "-n", help="Comma-separated list of names [default: a Scott Pilgrim's character]")
+@click.option("--as-a-pirate", "-p", count=True, help="Greet as a pirate")
+def greet(names, as_a_pirate):
+    """ Greet someone """
+    from helpers import greet
+
+    list_of_names = _parse_arg(names)
+    [click.echo(greet(name, as_a_pirate)) for name in list_of_names]
+    log().debug("All is good!")
+
+
+def _parse_arg(names):
+    return names.split(",") if names else [None]
